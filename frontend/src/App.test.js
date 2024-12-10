afc7433103e1564285ce2761cd956015a24004e4
pepengu <plotnikovdanya@gmail.com>
2024-12-10 05:48:12 +0300

    fix: incorect node.js workflow

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('testing tests', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/The date/i);
   expect(linkElement).toBeInTheDocument();
 });
