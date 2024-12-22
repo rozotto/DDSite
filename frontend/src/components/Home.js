be0e347e3739bd5d2047a4a223fb7209f97def61
rozotto <dimaegorov915@gmail.com>
2024-12-23 01:20:51 +0700

    fix: Home.js comments

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,21 +1,3 @@
-//import React from 'react';
-//import Navbar from './Navbar';
-//import './Home.css';
-//
-//const Home = () => {
-//    return (
-//        <div className="home-container">
-//            <Navbar />
-//
-//            <div className="content">
-//                <h1>Добро пожаловать!!</h1>
-//            </div>
-//        </div>
-//    );
-//};
-//
-//export default Home;
-
 import React from 'react';
 import Navbar from './Navbar';
 import './Home.css';
