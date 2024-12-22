f2886b2f18d44d251b51d18ebb2724dee6fbf44b
pepengu <plotnikovdanya@gmail.com>
2024-12-22 20:21:31 +0300

    style: python lint

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -9,11 +9,11 @@ class RegisterForm(forms.ModelForm):
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password', 'profile_photo']
+        fields = ["username", "email", "password", "profile_photo"]
 
     def clean_password_confirm(self):
-        password = self.cleaned_data.get('password')
-        password_confirm = self.cleaned_data.get('password_confirm')
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
         if password != password_confirm:
             raise forms.ValidationError("Passwords don't match")
         return password_confirm
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -26,12 +26,14 @@ class CustomUser(AbstractBaseUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
+    profile_photo = models.ImageField(
+        upload_to="profile_photos/", blank=True, null=True
+    )
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
 
     def __str__(self):
         return self.username
